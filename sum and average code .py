5bac6ddae5c6540c38459d60fc73d7dd42fc21e8
Elijahb44 <55414274+Elijahb44@users.noreply.github.com>
2019-12-14 13:41:45 -0500

    Add files via upload
    Fixed sum and average code.

---
+++ sum and average code .py
@@ -0,0 +1,14 @@
+
+numberlist = []
+for n in range(0,20):
+    num=int(input("Enter Number Here: "))
+    numberlist.append(numberlist)
+   
+sumint= 0
+average=0
+for n in range(0,21):
+    sumint = sumint + n
+
+average  = sumint / len(numberlist)
+print ("The sum of the numbers you entered is: ", sumint)
+print ("The average of the numbers you enterd is: ", average)
